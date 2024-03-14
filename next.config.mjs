01a97d22c6cd52c75530ba0004d54cac7dd4c362
ayakatsutsui <ayakatsutsui1128@gmail.com>
2024-03-15 04:52:42 +0900

    outディレクトリの作成

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
 
-export default nextConfig;
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    output: 'export', // 最新のNext.jsではこの設定は必要ないかもしれません
+    images: {
+      unoptimized: true, // 画像最適化を無効にする
+    },
+  };
+  
+  export default nextConfig;
+  
