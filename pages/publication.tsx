17d1c73986a9225cef8a257a5d9c6c2ccc48a964
Ayaka Tsutsui <82640338+angeleru1128@users.noreply.github.com>
2024-03-17 00:53:30 +0900

    Update publication.tsx
    change the link for home

--- pages/publication.tsx
+++ pages/publication.tsx
@@ -9,7 +9,7 @@ const Publication = () => {
   {/* バー */}
   <div className="flex justify-end font-mono text-base lg:w-3/4">
     <div className="space-x-5 p-5">
-      <Link href="/about">Home</Link>
+      <a href="https://ayakatsutsuidesu.com/">Home</a>
       <Link href="/about">About</Link>
       <a href="/document/AyakaTsutsui_Resume .pdf">CV</a>
     </div>
